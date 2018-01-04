024278cb3ba4c11d5bf0899308073a9fed7db278
John Robin <johnrobinlugtu@gmail.com>
2018-01-03 20:23:54 -0800

    Added conditionals to handle missing :subject and :to information. Error json message included

--- app/controllers/api/mails_controller.rb
+++ app/controllers/api/mails_controller.rb
@@ -3,6 +3,14 @@ class Api::MailsController < ApplicationController
 
     def send_mail
         @params = params
-        PersonalMailer.send_mail(@params[:to], @params[:subject], @params[:body]).deliver_later
+
+        if @params[:to].nil? || @params[:body].nil?
+            render 'api/mails/error_msg.json'
+        else
+            if @params[:subject].nil?
+                @params[:subject] = ""
+            end
+            PersonalMailer.send_mail(@params[:to], @params[:subject], @params[:body]).deliver_now
+        end
     end
 end
